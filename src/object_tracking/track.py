ccf133cdc79e823a589847cfc0ea789434e95ed3
Jerry Guo <jerrguo@gmail.com>
2021-02-14 16:42:57 -0500

    Update track.py

--- src/object_tracking/track.py
+++ src/object_tracking/track.py
@@ -4,24 +4,65 @@ import imutils
 import time
 import cv2
 from imutils.video import FPS
+import traceback
 
 OPENCV_OBJECT_TRACKERS = {
     "csrt": cv2.TrackerCSRT_create,
-    "kcf": cv2.TrackerKCF_create,
+    # "kcf": cv2.TrackerKCF_create,
     "boosting": cv2.TrackerBoosting_create,
     "mil": cv2.TrackerMIL_create,
-    "tld": cv2.TrackerTLD_create,
+    #"tld": cv2.TrackerTLD_create,
     "medianflow": cv2.TrackerMedianFlow_create,
-    "mosse": cv2.TrackerMOSSE_create,
+    # "mosse": cv2.TrackerMOSSE_create,
 }
 
-# Initialize bounding box of tracked object
-BB = None
-# Initialize FPS estimator
-fps = None
+class ObjectTracker:
 
-if __name__ == '__main__':
-    # Path to video for testing purposes
+    def __init__(self, model_name="mil"):
+        self.model_name = model_name
+
+    def _set_tracker(self):
+        self.tracker = OPENCV_OBJECT_TRACKERS[self.model_name]()
+
+
+    def start_tracker(self, bounding_box, image):
+        self._set_tracker()
+        self.tracker.init(image, bounding_box)
+        self.fps = FPS().start()
+
+    def track(self, bounding_box, image):
+        (H, W) = image.shape[:2] 
+        if not bounding_box:
+            raise("No bounding box found, rerun object detection...")
+
+        (success, box) = self.tracker.update(image)
+                
+        if success:
+            (x, y, w, h) = [int(v) for v in box]
+            cv2.rectangle(image, (x, y), (x + w, y + h), (0, 255, 0), 2)
+
+        # Update the FPS counter
+        self.fps.update()
+        self.fps.stop()
+
+        info = [
+            ("Tracker", self.model_name),
+            ("Success", "Yes" if success else "No"),
+            ("FPS", "{:.2f}".format(self.fps.fps())),
+        ]
+
+        # Loop over the info tuples and draw them on our frame
+        for (i, (k, v)) in enumerate(info):
+            text = "{}: {}".format(k, v)
+            cv2.putText(image, text, (10, H - ((i * 20) + 20)),
+                cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0, 0, 255), 2)
+
+        return image
+
+if __name__ == "__main__":
+
+    object_tracker = ObjectTracker()
+
     HOME_PATH = pathlib.Path(__file__).parent.absolute().as_posix()
     PATH_TO_TEST_VIDEOS = os.path.join(HOME_PATH, '../test_videos')
     PATH_TO_VIDEO = os.path.join(PATH_TO_TEST_VIDEOS, 'test1.mp4')
@@ -34,47 +75,16 @@ if __name__ == '__main__':
         if not cap.isOpened():
             raise IOError('OpenCV capture cannot be opened.')
 
+    bounding_box = None
+    
     while True:
-        ret, image_np = cap.read()
-        # image_np = cv2.cvtColor(image_np, cv2.COLOR_BGR2HSV)
-        # image_np = imutils.resize(image_np, width=500)
-        (H, W) = image_np.shape[:2]
-        
-        if BB is not None:
-            # Grab the new bounding box coordinates of the object
-            (success, box) = tracker.update(image_np)
-            # Check to see if the tracking was a success
-            if success:
-                (x, y, w, h) = [int(v) for v in box]
-                cv2.rectangle(image_np, (x, y), (x + w, y + h),
-                    (0, 255, 0), 2)
-            # Update the FPS counter
-            fps.update()
-            fps.stop()
-            # Initialize the set of information we'll be displaying on the frame
-            info = [
-                ("Tracker", 'KCF'),
-                ("Success", "Yes" if success else "No"),
-                ("FPS", "{:.2f}".format(fps.fps())),
-            ]
-            # Loop over the info tuples and draw them on our frame
-            for (i, (k, v)) in enumerate(info):
-                text = "{}: {}".format(k, v)
-                cv2.putText(image_np, text, (10, H - ((i * 20) + 20)),
-                    cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0, 0, 255), 2)
-        
-        # Show the output frame
-        cv2.imshow("Frame", image_np)
-
+        ret, image = cap.read()
+        if bounding_box is not None:
+            image = object_tracker.track(bounding_box, image)
         if cv2.waitKey(25) & 0xFF == ord("s"):
             # Select the bounding box of an object (x, y, width, height)
-            BB = cv2.selectROI("Frame", image_np, fromCenter=False,
-                showCrosshair=True)
-            print(BB)
-            # Initialize the tracker
-            tracker = OPENCV_OBJECT_TRACKERS['mil']()
-            tracker.init(image_np, BB)
-            fps = FPS().start()
-        
-    cap.release()
-    cv2.destroyAllWindows()
+            bounding_box = cv2.selectROI("Frame", image, fromCenter=False,
+            showCrosshair=True)
+            object_tracker.start_tracker(bounding_box, image)
+        # Show the output frame
+        cv2.imshow("Frame", image)
