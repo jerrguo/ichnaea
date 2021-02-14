4167b491e1878fd832dac7ecaee1d71392d891e3
Vandit Patel <patel.vandit1@gmail.com>
2021-02-14 16:51:03 -0500

    driver code for tracket and detection

--- src/driver.py
+++ src/driver.py
@@ -4,6 +4,7 @@ import cv2
 import tensorflow as tf
 
 from object_detection.detect import ObjectDetector
+from object_tracking.track import ObjectTracker
 
 # Path to video for testing purposes
 HOME_PATH = pathlib.Path(__file__).parent.absolute().as_posix()
@@ -21,23 +22,25 @@ if __name__ == '__main__':
 
     # Initialize object detector and tracker
     detector = ObjectDetector()
+    tracker = ObjectTracker()
     
     # Read the capture
     BB = None
     with detector.detection_graph.as_default():
         with tf.compat.v1.Session(graph=detector.detection_graph) as sess:
             while True:
+                print("new image...")
                 ret, image_np = cap.read()
                 # If there is no bounding box, run the detection algorithm, otherwise track
                 if not BB:
                     boxes, scores, classes, num_detections = detector.detect_objects(sess, image_np)
-                    # print(boxes)
-                    # print(scores)
-                    # print(classes)
-                    # print(num_detections)
                     # TODO: Set bounding box used for tracking
+                    BB = boxes[0][0]
+                    tracker.start_tracker(BB, image_np)
                 else:
-                    pass
+                    image_np = tracker.keep_tracking(BB, image_np)
+                
+                cv2.imshow("Frame", image_np)
             
     cap.release()
     cv2.destroyAllWindows()
--- src/object_tracking/track.py
+++ src/object_tracking/track.py
@@ -8,11 +8,11 @@ import traceback
 
 OPENCV_OBJECT_TRACKERS = {
     "csrt": cv2.TrackerCSRT_create,
-    # "kcf": cv2.TrackerKCF_create,
-    "boosting": cv2.TrackerBoosting_create,
+    "kcf": cv2.TrackerKCF_create,
+    # "boosting": cv2.TrackerBoosting_create,
     "mil": cv2.TrackerMIL_create,
     #"tld": cv2.TrackerTLD_create,
-    "medianflow": cv2.TrackerMedianFlow_create,
+    # "medianflow": cv2.TrackerMedianFlow_create,
     # "mosse": cv2.TrackerMOSSE_create,
 }
 
