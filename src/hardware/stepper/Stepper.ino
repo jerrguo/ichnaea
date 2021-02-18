0eb3cfb0c46959fc3b67340bd3fd4a1d14b71271
Andrew Pai <andrewpai148@hotmail.com>
2021-02-18 01:22:27 -0500

    Update Stepper.ino
    Comment out debug print

--- src/hardware/stepper/Stepper.ino
+++ src/hardware/stepper/Stepper.ino
@@ -37,7 +37,6 @@ void loop() {
 
 
   if(digitalRead(NANO_ENA) == HIGH){
-    Serial.print("test\n");
     // Get direction
     value = digitalRead(NANO_DIR);
     digitalWrite(STEP_DIR,value);
@@ -57,21 +56,21 @@ void loop() {
     delayMicroseconds(udelay);
 
     
-    int ena = digitalRead(NANO_ENA);
-    int dir = digitalRead(NANO_DIR);
-    Serial.print("Speed: ");
-    int spd [] = {0, 0, 0, 0};
-    for (int x = 0; x < 4; x++){
-      spd[x] = digitalRead(NANO_SPEED[x]);
-      Serial.print(spd[x]);
-    }
+    //int ena = digitalRead(NANO_ENA);
+    //int dir = digitalRead(NANO_DIR);
+    //Serial.print("Speed: ");
+    //int spd [] = {0, 0, 0, 0};
+    //for (int x = 0; x < 4; x++){
+    //  spd[x] = digitalRead(NANO_SPEED[x]);
+    //  Serial.print(spd[x]);
+    //}
   
-    Serial.print(" ENA: ");
-    Serial.print(ena);
-    Serial.print(" DIR: ");
-    Serial.print(dir);
+    //Serial.print(" ENA: ");
+    //Serial.print(ena);
+    //Serial.print(" DIR: ");
+    //Serial.print(dir);
     
-    Serial.print("\n");
+    //Serial.print("\n");
     }
 
 }
