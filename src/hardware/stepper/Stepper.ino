1740ed20ede50b983964a738e8ad502fc3cd9772
Shivam Dharme <shdh8888@yahoo.ca>
2021-02-18 21:19:54 -0500

    New working copy of Arduino program

--- src/hardware/stepper/Stepper.ino
+++ src/hardware/stepper/Stepper.ino
@@ -14,7 +14,8 @@ int NANO_SPEED[] = {4, 5, 6, 7}; //2^4 = 16 speed control
 int NANO_DIR = 8;
 int NANO_ENA = 9;
 
-int value;
+int nano_ena_flag;
+int nano_dir_value;
 
 int udelay = 0; //Delay between pulses
 
@@ -34,43 +35,44 @@ void setup() {
 }
 
 void loop() {
-
-
-  if(digitalRead(NANO_ENA) == HIGH){
+  nano_ena_flag = digitalRead(NANO_ENA);
+  Serial.print(" ENA: ");
+  Serial.print(nano_ena_flag);
+  if(nano_ena_flag == HIGH){
     // Get direction
-    value = digitalRead(NANO_DIR);
-    digitalWrite(STEP_DIR,value);
+    nano_dir_value = digitalRead(NANO_DIR);
+    digitalWrite(STEP_DIR,nano_dir_value);
+
+    Serial.print(" DIR: ");
+    Serial.print(nano_dir_value);
 
     // Get speed
-    udelay =  750;
-    for (int x = 0; x < 4; x++){
-      if(digitalRead(NANO_SPEED[x]) == HIGH){
-        udelay = udelay - 150;
-      }
-    }
-    
-    digitalWrite(STEP_ENA,HIGH);
-    digitalWrite(STEP_PUL,HIGH);
-    delayMicroseconds(udelay);
-    digitalWrite(STEP_PUL,LOW);
-    delayMicroseconds(udelay);
+//    udelay = 601;
+//    Serial.print(" Speed: ");
+//    int spd [] = {0, 0, 0, 0};
+//    for (int x = 0; x < 4; x++){
+//      spd[x] = digitalRead(NANO_SPEED[x]);
+//      Serial.print(spd[x]);
+//      if(digitalRead(NANO_SPEED[x]) == HIGH){
+//        udelay = udelay - 150;
+//      }
+//    }
 
+//    Serial.print(" got udelay: ");
+//    Serial.print(udelay);
+
+    int numSteps = 20;
+
+    digitalWrite(STEP_ENA,HIGH);
+    for( int x=0; x<numSteps; x++){
+        digitalWrite(STEP_PUL,HIGH);
+//        delayMicroseconds(udelay);
+        digitalWrite(STEP_PUL,LOW);
+//        delayMicroseconds(udelay);
+    }
+//    digitalWrite(STEP_ENA,LOW);
     
-    //int ena = digitalRead(NANO_ENA);
-    //int dir = digitalRead(NANO_DIR);
-    //Serial.print("Speed: ");
-    //int spd [] = {0, 0, 0, 0};
-    //for (int x = 0; x < 4; x++){
-    //  spd[x] = digitalRead(NANO_SPEED[x]);
-    //  Serial.print(spd[x]);
-    //}
-  
-    //Serial.print(" ENA: ");
-    //Serial.print(ena);
-    //Serial.print(" DIR: ");
-    //Serial.print(dir);
-    
-    //Serial.print("\n");
+    Serial.print("\n");
     }
 
 }
