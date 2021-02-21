38a5ca57a9d7099fc3e75409dbb6e16000a0834e
Vandit <patel.vandit1@gmail.com>
2021-02-20 21:39:56 -0500

    custom build for openCV works

--- src/object_tracking/track.py
+++ src/object_tracking/track.py
@@ -59,6 +59,8 @@ class ObjectTracker:
 
         return image, success
 
+plz = 'nvarguscamerasrc sensor-id=0 ! video/x-raw(memory:NVMM), width=1920, height=1080, format=(string)NV12, framerate=21/1 ! nvvidconv flip-method=0 ! video/x-raw, width=960, height=616, format=(string)BGRx ! videoconvert ! video/x-raw, format=(string)BGR ! appsink wait-on-eos=false max-buffers=1 drop=True'
+plz0 = 'nvarguscamerasrc sensor-id=0 ! "video/x-raw(memory:NVMM),width=1920,height=1080,framerate=25/1" ! nvvidconv ! nvoverlaysink ! format=(string)BGRx ! videoconvert ! video/x-raw, format=(string)BGR ! appsink wait-on-eos=false max-buffers=1 drop=True'
 if __name__ == "__main__":
 
     object_tracker = ObjectTracker()
@@ -69,7 +71,7 @@ if __name__ == "__main__":
 
     # Initialize OpenCV capture
     capture_index = PATH_TO_VIDEO
-    cap = cv2.VideoCapture(capture_index)
+    cap = cv2.VideoCapture(plz, cv2.CAP_GSTREAMER)
     if not cap.isOpened():
         cap.open(capture_index)
         if not cap.isOpened():
